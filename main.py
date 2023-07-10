b4c96fb9c7cf5a637e18e861d656379b3b7a3395
ReimuJF <slavalang@gmail.com>
2023-07-10 17:44:18 +0500

    1007 update
    - Added new tag combinations
    - Added search for solo tags
    - Split big function on lesser ones

--- main.py
+++ main.py
@@ -6,43 +6,56 @@ import tkinter as tk
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 # if you have tesseract error
 
-cool_combo_4 = [('AoE', 'Slow'), ('Caster', 'Slow'), ('DP-Recovery', 'Healing'),
+cool_combo_4 = {('AoE', 'Slow'), ('Caster', 'Slow'), ('DP-Recovery', 'Healing'),
                 ('DPS', 'Slow'), ('Guard', 'Slow'), ('Healing', 'Support'),
                 ('Melee', 'Slow'), ('Ranged', 'Survival'), ('Vanguard', 'Healing'),
                 ('AoE', 'Debuff'), ('Caster', 'Debuff'), ('Sniper', 'Debuff'), ('DPS', 'Support'),
-                ('Sniper', 'Survival'), ('Sniper', 'Slow')]
+                ('Sniper', 'Survival'), ('Sniper', 'Slow'), ('Debuff', 'Ranged')}
 
-cool_combo_5 = [('DPS', 'Healing'), ('Healing', 'Slow'), ('Fast-Redeploy', 'Debuff'),
+cool_combo_5 = {('DPS', 'Healing'), ('Healing', 'Slow'), ('Fast-Redeploy', 'Debuff'),
                 ('DP-Recovery', 'Support'), ('DPS', 'Shift'), ('Defender', 'Shift'), ('Defender', 'DPS'),
                 ('Defender', 'Survival'), ('Defense', 'DPS'), ('Defense', 'Shift'), ('Defense', 'Survival'),
                 ('Melee', 'Debuff'), ('Shift', 'Slow'), ('Specialist', 'Slow'), ('Survival', 'Specialist'),
                 ('Supporter', 'DPS'), ('Supporter', 'Debuff'), ('Vanguard', 'Support'),
-                ('Caster', 'Healing'), ('Defense', 'Guard')]
+                ('Caster', 'Healing'), ('Defense', 'Guard'), ('AoE', 'Debuff'),
+                ('Nuker', 'Sniper'), ('Nuker', 'Ranged'), ('Specialist', 'DPS')}
 
+guaranteed = {'Top', 'Nuker', 'Specialist', 'Summon', 'Support',
+              'Debuff', 'Crowd-Control', 'Senior', 'Shift', 'Robot', 'Fast-Redeploy'}
 
-def calculate_tags():
+def get_image():
     txt_edit.delete("1.0", tk.END)
-
     try:
         image_in_buffer = ImageGrab.grabclipboard()
         image_in_buffer.save('1.png', 'PNG')
         image_to_invert = Image.open('1.png')
         image_to_invert = image_to_invert.convert('L')
         inverted_image = ImageOps.invert(image_to_invert)
-        # image_to_invert = image_to_invert.convert('1')
         inverted_image.save('1.png', 'PNG')
         image_to_con = Image.open('1.png')
         enhancer = ImageEnhance.Contrast(image_to_con)
-        image_out = enhancer.enhance(2)
+        image_out = enhancer.enhance(3)
         image_out.save('1.png', 'PNG')
     except AttributeError:
         pass
+    read_image()
 
-    tag_list = (pytesseract.pytesseract.image_to_string(Image.open('1.png'), config='--psm 12')).strip(',.-_+~`')
+def read_image():
+    tag_list = (pytesseract.pytesseract.image_to_string(Image.open('1.png'), config='--psm 12')).replace(',','')
     # tag_list = 'Debuff Supporter Caster Defense Slow'
-    txt_edit.insert(tk.END, f'{tag_list.split()}\n')
-    perm_tags = list(permutations(tag_list.split(), 2))
+    get_combinations(tag_list.split())
+def get_combinations(tag_list):
+    txt_edit.insert(tk.END, f'{tag_list}\n')
     flag = False
+    for i in tag_list:
+        if i == 'Top':
+            txt_edit.insert(tk.END, f'WOW! {i} Operator! You get free 6 star!\n')
+            flag = True
+        elif i in guaranteed:
+            txt_edit.insert(tk.END, f'Solo tag {i}. 5 or 4 star.\n')
+            flag = True
+    perm_tags = list(permutations(tag_list, 2))
+
 
     for i in perm_tags:
         if i in cool_combo_5:
@@ -51,24 +64,23 @@ def calculate_tags():
         elif i in cool_combo_4:
             txt_edit.insert(tk.END, f'4 star {i}\n')
             flag = True
-    else:
-        if not flag:
-            txt_edit.insert(tk.END, '\nno cool tags')
+    if not flag:
+        txt_edit.insert(tk.END, '\nno cool tags')
 
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.title('Arknights Tag Calculator')
+    window.geometry('550x150+300+500')
+    window.rowconfigure(0, minsize=100, weight=1)
+    window.columnconfigure(1, minsize=100, weight=1)
+    window.resizable(width=False, height=False)
 
-window = tk.Tk()
-window.title('Arknights Tag Calculator')
-window.geometry('550x150+300+500')
-window.rowconfigure(0, minsize=100, weight=1)
-window.columnconfigure(1, minsize=100, weight=1)
-window.resizable(width=False, height=False)
+    frame_buttons = tk.Frame(window, bd=2)
 
-frame_buttons = tk.Frame(window, bd=2)
+    txt_edit = tk.Text(window)
+    txt_edit.grid(row=0, column=1, sticky='wn')
 
-txt_edit = tk.Text(window)
-txt_edit.grid(row=0, column=1, sticky='wn')
-
-btn_calc = tk.Button(frame_buttons, text='Calculate Tags', command=calculate_tags)
-btn_calc.grid(row=0, column=0, sticky='we', padx=0, pady=5)
-frame_buttons.grid(row=0, column=0, sticky='ns')
-window.mainloop()
+    btn_calc = tk.Button(frame_buttons, text='Calculate Tags', command=get_image)
+    btn_calc.grid(row=0, column=0, sticky='we', padx=0, pady=5)
+    frame_buttons.grid(row=0, column=0, sticky='ns')
+    window.mainloop()
